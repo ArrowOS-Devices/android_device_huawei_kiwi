54873da73fed26f3d4ada846f488eae8b647383b
Ad!thya R <gh0strider.2k18.reborn@gmail.com>
2018-07-18 23:02:04 +0530

    kiwi: vendorsetup: Enable SDCLANG with optimized flags
    - Enable O3
    - Enable automatic vectorization
    - Add -Wno-vectorizer-no-neon -Wno-user-defined-warnings to hide few warnings
    
    Signed-off-by: Ad!thya R <gh0strider.2k18.reborn@gmail.com>
    
    Co-authored-by: Alex Naidis <alex.naidis@linux.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,4 @@
+export TARGET_USE_SDCLANG=true
+export SDCLANG_FLAGS="-O3 -fvectorize-loops -Wno-vectorizer-no-neon -Wno-user-defined-warnings"
+export SDCLANG_FLAGS_2="-O3 -fvectorize-loops -Wno-vectorizer-no-neon -Wno-user-defined-warnings"
+export SDCLANG_COMMON_FLAGS="-O3 -fvectorize-loops -Wno-vectorizer-no-neon -Wno-user-defined-warnings"
